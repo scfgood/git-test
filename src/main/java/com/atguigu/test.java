b9f6b28c2a20d2f5453e894f6dfd818f611197fc
scf <2631131676@qq.com>
2021-09-28 21:10:32 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/test.java
+++ src/main/java/com/atguigu/test.java
@@ -5,5 +5,6 @@ public class test {
         System.out.println("hello git");
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("i'm good");
     }
 }
